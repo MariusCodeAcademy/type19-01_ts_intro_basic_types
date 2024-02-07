a3a1dbdb1a0ad1f08507522006bdc4eaeec2557f
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-07 10:56:38 +0200

    map

--- public/arrays.js
+++ public/arrays.js
@@ -5,3 +5,12 @@ let colors = ['red', 'green', 'blue'];
 colors.push('5');
 let mixed = ['blue', 'green'];
 mixed.push(5);
+const uppperCol = colors.map((color) => color.toUpperCase());
+console.log('uppperCol ===', uppperCol);
+const numsArr = [1, 5, -10, -5, 50, 10];
+// atfiltruoti skaicius didesnius uz 3 i kintamaji largerThan3
+const filtered = numsArr
+    .filter((sk) => sk > 3)
+    .map((sk) => sk.toString());
+// surasyti vidine anf tipus ir grazinima
+console.log('filtered ===', filtered);
--- src/arrays.ts
+++ src/arrays.ts
@@ -8,3 +8,16 @@ colors.push('5');
 let mixed: (string | number)[] = ['blue', 'green'];
 
 mixed.push(5);
+
+const uppperCol: string[] = colors.map((color: string): string => color.toUpperCase());
+
+console.log('uppperCol ===', uppperCol);
+
+const numsArr: number[] = [1, 5, -10, -5, 50, 10];
+
+// atfiltruoti skaicius didesnius uz 3 i kintamaji largerThan3
+const filtered: string[] = numsArr
+  .filter((sk: number): boolean => sk > 3)
+  .map((sk) => sk.toString());
+// surasyti vidine anf tipus ir grazinima
+console.log('filtered ===', filtered);
