9f4ddd0a6c259ccf17e72568cc0ed0b2958c3dee
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-07 11:04:34 +0200

    obj

---
+++ public/obj.js
@@ -0,0 +1,18 @@
+"use strict";
+const user1 = {
+    id: 1,
+    name: 'James',
+    isAdmin: true,
+};
+user1.isAdmin = false;
+// user1.town = 'London'; // negalima
+const user2 = {
+    id: 2,
+    name: 'Jane',
+    isAdmin: false,
+};
+function userInfo(userObj) {
+    // userObj.name;
+    // userObj.name is admin
+    // userObj.name is not admin
+}
