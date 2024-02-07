9eb9ea59898116439409d7a0264cc565269b881a
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-07 10:04:50 +0200

    fn

---
+++ public/fn.js
@@ -0,0 +1,12 @@
+"use strict";
+function greet(name, weekDay) {
+    console.log('Hello ', name, weekDay);
+}
+// greet(5);
+// greet('James');
+greet('James', 2);
+// parasyti funkcija sum(4, 5) ir grazina ju suma
+// uzdeti tipus argumentams ir funkcijai
+// isbandyti fn su 2 random reikmem
+// pasiimti reiksmes ir promt() ir gauti 2 skaiciu suma
+// prompt('iveskite reiksme');
---
+++ src/fn.ts
@@ -0,0 +1,14 @@
+function greet(name: string, weekDay: number): void {
+  console.log('Hello ', name, weekDay);
+}
+
+// greet(5);
+// greet('James');
+greet('James', 2);
+
+// parasyti funkcija sum(4, 5) ir grazina ju suma
+// uzdeti tipus argumentams ir funkcijai
+// isbandyti fn su 2 random reikmem
+
+// pasiimti reiksmes ir promt() ir gauti 2 skaiciu suma
+// prompt('iveskite reiksme');
