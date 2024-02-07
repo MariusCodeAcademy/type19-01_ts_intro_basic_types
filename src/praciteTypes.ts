8b88dd248a350307f5e6f6b4bf9b79eb49afcebf
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-07 11:33:05 +0200

    uzd

---
+++ src/praciteTypes.ts
@@ -0,0 +1,8 @@
+// sukurti Customer tipa
+
+// Customer tures name, age, buySum, isDriver
+
+// sukuriam Customer masyva su 5 customer objektais
+
+// parasyti funkcija, kuri ima masyva kai argumenta
+// grazina visu buySum sumu sumas
