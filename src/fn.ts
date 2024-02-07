02cd62ab31ce6abf2b73bd0a354bc6d617c2aa62
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-07 10:35:50 +0200

    prompt

--- src/fn.ts
+++ src/fn.ts
@@ -7,8 +7,33 @@ function greet(name: string, weekDay: number): void {
 greet('James', 2);
 
 // parasyti funkcija sum(4, 5) ir grazina ju suma
+function sum(x: number, y: number): number {
+  console.log(x + y);
+  return x + y;
+}
+sum(4, 5);
 // uzdeti tipus argumentams ir funkcijai
 // isbandyti fn su 2 random reikmem
 
+let num1str = prompt('iveskite reiksme 1');
+let num2str = prompt('iveskite reiksme 2');
+
+let num1 = Number(num1str);
+let num2 = Number(num2str);
+
+sum(num1, num2);
+
+function flow(): void {
+  // isistikinam kad vartotojas nepaspaude cancel
+  if (num1str === null || num2str === null) {
+    return;
+  }
+
+  sum(+num1str, +num2str);
+}
+flow();
+
 // pasiimti reiksmes ir promt() ir gauti 2 skaiciu suma
 // prompt('iveskite reiksme');
+
+// makePrice(50) => '$50.00'
