924ec3770f1ac3e81af501af83faa59d06fbcc75
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-07 10:40:13 +0200

    makePrice

--- public/fn.js
+++ public/fn.js
@@ -13,19 +13,25 @@ function sum(x, y) {
 sum(4, 5);
 // uzdeti tipus argumentams ir funkcijai
 // isbandyti fn su 2 random reikmem
-let num1str = prompt('iveskite reiksme 1');
-let num2str = prompt('iveskite reiksme 2');
-let num1 = Number(num1str);
-let num2 = Number(num2str);
-sum(num1, num2);
-function flow() {
-    // isistikinam kad vartotojas nepaspaude cancel
-    if (num1str === null || num2str === null) {
-        return;
-    }
-    sum(+num1str, +num2str);
-}
-flow();
+// let num1str = prompt('iveskite reiksme 1');
+// let num2str = prompt('iveskite reiksme 2');
+// let num1 = Number(num1str);
+// let num2 = Number(num2str);
+// sum(num1, num2);
+// function flow(): void {
+//   // isistikinam kad vartotojas nepaspaude cancel
+//   if (num1str === null || num2str === null) {
+//     return;
+//   }
+//   sum(+num1str, +num2str);
+// }
+// flow();
 // pasiimti reiksmes ir promt() ir gauti 2 skaiciu suma
 // prompt('iveskite reiksme');
 // makePrice(50) => '$50.00'
+function makePrice(value) {
+    const rez = `$${value.toFixed(2)}`;
+    console.log('rez ===', rez);
+    return rez;
+}
+makePrice(100);
--- src/fn.ts
+++ src/fn.ts
@@ -15,25 +15,33 @@ sum(4, 5);
 // uzdeti tipus argumentams ir funkcijai
 // isbandyti fn su 2 random reikmem
 
-let num1str = prompt('iveskite reiksme 1');
-let num2str = prompt('iveskite reiksme 2');
+// let num1str = prompt('iveskite reiksme 1');
+// let num2str = prompt('iveskite reiksme 2');
 
-let num1 = Number(num1str);
-let num2 = Number(num2str);
+// let num1 = Number(num1str);
+// let num2 = Number(num2str);
 
-sum(num1, num2);
+// sum(num1, num2);
 
-function flow(): void {
-  // isistikinam kad vartotojas nepaspaude cancel
-  if (num1str === null || num2str === null) {
-    return;
-  }
+// function flow(): void {
+//   // isistikinam kad vartotojas nepaspaude cancel
+//   if (num1str === null || num2str === null) {
+//     return;
+//   }
 
-  sum(+num1str, +num2str);
-}
-flow();
+//   sum(+num1str, +num2str);
+// }
+// flow();
 
 // pasiimti reiksmes ir promt() ir gauti 2 skaiciu suma
 // prompt('iveskite reiksme');
 
 // makePrice(50) => '$50.00'
+
+function makePrice(value: number): string {
+  const rez: string = `$${value.toFixed(2)}`;
+  console.log('rez ===', rez);
+  return rez;
+}
+
+makePrice(100);
